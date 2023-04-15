235a43b57819610808999ad3289b349b9896ce5c
Johannes Zottele <official@johannes-zottele.at>
2023-04-15 13:37:04 +0200

    Add Server Config

--- Backend/Sources/App/configure.swift
+++ Backend/Sources/App/configure.swift
@@ -2,6 +2,9 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
+    // server configs
+    app.http.server.configuration.hostname = "0.0.0.0" 
+
     // configure network manager
     let networkManager = NetworkManager()
     networkManager.configureRoutes(routes: app.routes)
