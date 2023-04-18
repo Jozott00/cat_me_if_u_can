06bdc6fd0b00eac597db50c99db073ae3e222d0e
flofriday <flohacksfriday@gmail.com>
2023-04-18 17:24:35 +0200

    Add a more sophisticated tunnel generation algorithm
    It is still not perfect, but it will be more than enough for now.
    However, the entries will cluster in the middle of the virtual circle in
    which they get generated. Only testing inside the game will tell if
    it is too obvious that they belong to the same tunnel.

--- Backend/Sources/App/Controllers/TunnelGenerator.swift
+++ Backend/Sources/App/Controllers/TunnelGenerator.swift
@@ -4,23 +4,44 @@ import Shared
 /// Generate the tunnel layout for a new round of the game
 /// - Returns: the list of tunnels
 func generateTunnels() -> [Tunnel] {
-    let NUM_TUNNELS = 5
-
     var tunnels: [Tunnel] = []
-    for _ in 1 ... NUM_TUNNELS {
-        let numEntries = Int.random(in: 2 ... 4)
+    var entryPositions: [Position] = []
+
+    for _ in 1 ... Constants.TUNNELS_NUM {
+        // Generate how many entries the tunnel should have
+        let numEntries = Int.random(in: 2 ... Constants.MAX_ENTRIES)
+
+        // First we create a virtual center point for our tunnel, around this
+        // center we will generate the entries of the tunnel (with polar
+        // translation)
+        // FIXME: This is not optimal, because they will cluster in the middle
+        let padding = Constants.FIELD_LENGTH / 10
+        let virtualCenter = Position(
+            x: Int.random(in: padding ... (Constants.FIELD_LENGTH - padding)),
+            y: Int.random(in: padding ... (Constants.FIELD_LENGTH - padding))
+        )
 
         let entries = (2 ... numEntries).map { _ in
-            Entry(
-                id: UUID(),
-                position: Position(
-                    x: Int.random(in: 100 ... 900),
-                    y: Int.random(in: 100 ... 900)
+            // Optimistically generate a position for this entry and check if
+            // it is viable (not too close to any other entry of any of the
+            // other tunnels)
+            var position: Position
+            repeat {
+                position = Position(position: virtualCenter)
+                position.translate(
+                    r: Int.random(in: (Constants.ENTRY_SIZE * 2) ... (Constants.ENTRIES_MAX_DISTANCE / 2)),
+                    phi: Float64.random(in: 0 ... 2) * Float64.pi
                 )
-            )
+            } while entryPositions.contains { ep in
+                ep.distance(to: position) < Constants.ENTRIES_MIN_DISTANCE
+            }
+
+            entryPositions.append(position)
+            return Entry(id: UUID(), position: position)
         }
-        let tunnel = Tunnel(id: UUID(), entries: entries)
-        tunnels.append(tunnel)
+
+        tunnels.append(Tunnel(id: UUID(), entries: entries))
     }
+
     return tunnels
 }
--- Shared/Sources/Shared/Position.swift
+++ Shared/Sources/Shared/Position.swift
@@ -15,4 +15,25 @@ public class Position: Codable {
         self.x = x
         self.y = y
     }
+
+    public init(position: Position) {
+        x = position.x
+        y = position.y
+    }
+
+    // Move the position by x and y cartesian coordinates
+    public func translate(x: Int, y: Int) {
+        self.x += x
+        self.y += y
+    }
+
+    // Move the position by radius and angle (polar coordinates)
+    public func translate(r: Int, phi: Float64) {
+        x += Int(Float64(r) * cos(phi))
+        y += Int(Float64(r) * sin(phi))
+    }
+
+    public func distance(to: Position) -> Int64 {
+        return Int64(sqrt(pow(Float64(to.x - x), 2.0) + pow(Float64(to.y - y), 2.0)))
+    }
 }
