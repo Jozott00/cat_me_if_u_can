16e46dda59abcf492fce966c648d6e3ad17aee10
Paul Pinter <paul.pinter@fastmail.com>
2023-04-30 22:54:43 +0200

    Omit alert sound when pressing down keys

--- Client/Client/Views/Demo/WebsocketContentView.swift
+++ Client/Client/Views/Demo/WebsocketContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct WebsocketContentView: View {
+  init() {
+    // omits alert sound when pressing down keys
+    NSEvent.addLocalMonitorForEvents(matching: .keyDown) { _ in return nil }
+  }
   var body: some View {
     WebsocketInnerView().environmentObject(GameSession.data)
   }
