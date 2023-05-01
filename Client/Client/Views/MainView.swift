ba90ec67737f66dba4e01207558acc7a67f59157
Johannes Zottele <official@johannes-zottele.at>
2023-05-01 16:17:15 +0200

    Working state

--- Client/Client/Views/MainView.swift
+++ Client/Client/Views/MainView.swift
@@ -10,22 +10,25 @@ import SwiftUI
 
 struct MainView: View {
     @State private var currentView: Int = 1
-    @State private var gameLayout: ProtoGameLayout = ProtoGameLayout(exits: [
+    @State private var gameLayout: ProtoGameLayout = .init(exits: [
         ProtoExit(exitID: "testid1", position: Position(x: 1, y: 1)),
         ProtoExit(exitID: "testid2", position: Position(x: 100, y: 10)),
         ProtoExit(exitID: "testid3", position: Position(x: 40, y: 700)),
         ProtoExit(exitID: "testid4", position: Position(x: 390, y: 210)),
     ])
-    @State private var gameState: ProtoGameState = ProtoGameState(
+    @State private var gameState: ProtoGameState = .init(
         mice: [ProtoMouse(mouseID: "Mouse1", position: Position(x: 100, y: 10), state: "north")],
         cats: [ProtoCat(playerID: "player1", position: Position(x: 40, y: 700))]
     )
 
+    @StateObject var data: GameData = .init()
+    @StateObject var game: GameSession = .init()
+
     @State private var username: String = RandomTextSelector(fileName: "usernames")
         .getRandomListElement()
 
-    var xCord: Int = 1
-    var yCord: Int = 1
+    var xCord: Double = 1
+    var yCord: Double = 1
 
     // timer bit messy in structs, just for testing
     @State var timerLogic: TimerLogic!
@@ -41,7 +44,7 @@ struct MainView: View {
             self.timer = Timer.scheduledTimer(
                 timeInterval: 0.01,
                 target: self,
-                selector: #selector(timerTicked),
+                selector: #selector(self.timerTicked),
                 userInfo: nil,
                 repeats: true
             )
@@ -69,7 +72,7 @@ struct MainView: View {
         self.yCord += 1
         self.xCord += 1
         self.gameState.mice = [
-            ProtoMouse(mouseID: "Mouse1", position: Position(x: xCord, y: yCord), state: "north")
+            ProtoMouse(mouseID: "Mouse1", position: Position(x: self.xCord, y: self.yCord), state: "north"),
         ]
     }
 
@@ -79,6 +82,8 @@ struct MainView: View {
         // 4 main view with global navigation
         if currentView == 1 {
             LobbyView(username: $username, currentView: $currentView)
+                .environmentObject(data)
+                .environmentObject(game)
         }
         else if currentView == 2 {
             LoadingScreenView(
@@ -86,7 +91,8 @@ struct MainView: View {
             )
         }
         else if currentView == 3 {
-            BoardView(currentView: $currentView, gameLayout: $gameLayout, gameState: $gameState)
+            BoardView(currentView: $currentView)
+                .environmentObject(data)
                 .onAppear { startTimer() }
                 .onDisappear { endTimer() }
         }
@@ -97,6 +103,5 @@ struct MainView: View {
             // maybe hier noch gescheite exception einbauen, kein plan wie das mit throws functioniert in ner view
             let _ = print(UIError.NavigationError)
         }
-
     }
 }
