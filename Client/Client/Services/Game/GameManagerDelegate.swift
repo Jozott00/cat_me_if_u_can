eaec9883424f02ce9b7c5ce9ab4ea244f1ef445c
Johannes Zottele <official@johannes-zottele.at>
2023-05-03 14:09:36 +0200

    Log instead of crash

--- Client/Client/Services/Game/GameManagerDelegate.swift
+++ Client/Client/Services/Game/GameManagerDelegate.swift
@@ -11,49 +11,49 @@ import Shared
 
 /// Responsible handling WS Messages concerning the game
 class GameManagerDelegate: WebSocketDelegate {
-  private let log = Logger(label: "GameManagerDelegate")
-  let data: GameData = GameSession.data
+    private let log = Logger(label: "GameManagerDelegate")
+    let data: GameData = GameSession.data
 
-  func onConnected() {
-    log.info("Connected")
-  }
-
-  func onDisconnected() {
-    log.info("Disconnected")
-  }
-
-  func onError(error: Error) {
-    log.error("WS Error \(error.localizedDescription)")
-  }
-
-  func onMessage(msg: String) {
-    if let protocolMsg = msg.toProtoMsg() {
-      if case let .update(update) = protocolMsg.body {
-        switch update.data {
-        case let .gameStart(layout):
-          DispatchQueue.main.async {
-            self.data.gameLayout = layout
-          }
-        case let .gameState(state):
-          DispatchQueue.main.async {
-            self.data.gameState = state
-          }
-        case let .joinAck(id):
-          log.info("ack: \(id)")
-          // Start tracking player movement
-          KeyboardManager.start()
-        case .lobbyUpdate(users: _, gameRunning: _):
-          fatalError("Not implemented")
-        case .scoreboard(board: _):
-          fatalError("Not implemented")
-        case .gameEnd(score: _):
-          fatalError("Not implemented")
-        }
-      } else {
-        log.error("The JSON does not contain an update: \(msg)")
-      }
-    } else {
-      log.error("Error decoding JSON string.")
+    func onConnected() {
+        log.info("Connected")
     }
-  }
+
+    func onDisconnected() {
+        log.info("Disconnected")
+    }
+
+    func onError(error: Error) {
+        log.error("WS Error \(error.localizedDescription)")
+    }
+
+    func onMessage(msg: String) {
+        if let protocolMsg = msg.toProtoMsg() {
+            if case let .update(update) = protocolMsg.body {
+                switch update.data {
+                case let .gameStart(layout):
+                    DispatchQueue.main.async {
+                        self.data.gameLayout = layout
+                    }
+                case let .gameState(state):
+                    DispatchQueue.main.async {
+                        self.data.gameState = state
+                    }
+                case let .joinAck(id):
+                    log.info("ack: \(id)")
+                    // Start tracking player movement
+                    KeyboardManager.start()
+                case .lobbyUpdate(users: _, gameRunning: _):
+                    log.error("LobbyUpdate is not implemented")
+                case .scoreboard(board: _):
+                    log.error("Scoreboard is not implemented")
+                case .gameEnd(score: _):
+                    log.error("GameEnd is not implemented")
+                }
+            } else {
+                log.error("The JSON does not contain an update: \(msg)")
+            }
+        } else {
+            log.error("Error decoding JSON string.")
+        }
+    }
 }
