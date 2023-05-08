7013803c17a481a986a51fea0e90840ae9244082
Johannes Zottele <official@johannes-zottele.at>
2023-05-08 10:55:15 +0200

    Fix NP issue

--- Backend/Sources/App/Entities/GameState.swift
+++ Backend/Sources/App/Entities/GameState.swift
@@ -55,9 +55,7 @@ actor GameState {
             case .catchable: miceLeft += 1
             case .reachedGoal: miceMissed += 1
             case let .catched(by: cat):
-                // FIXME: Causes NP exception
-                // Maybe add(cat was not used apropreadly
-                scores[cat]! += 1
+                scores[cat] = (scores[cat] ?? 0) + 1
             }
         }
 
