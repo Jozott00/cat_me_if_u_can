ee4759f9039c5b4c5413cc33d78f5eb6268a67a1
flofriday <flohacksfriday@gmail.com>
2023-06-01 23:30:02 +0200

    Add header to connection screen and enter to continue

--- Client/Client/Views/CMIYC-Lobby/LobbyView.swift
+++ Client/Client/Views/CMIYC-Lobby/LobbyView.swift
@@ -11,15 +11,30 @@ struct LobbyView: View {
   @Binding var username: String
   @Binding var currentView: MainViews  // current view being passed to view
 
+  func connect() {
+    if username.isEmpty {
+      return
+    }
+    GameSession.join(userName: username)
+    currentView = .loadingScreen
+  }
+
   var body: some View {
-    VStack(alignment: .leading) {
-      
+    VStack {
+      Text("Join the server")
+        .font(.largeTitle)
+        .bold()
+
       HStack {
         TextField(
           "Username",
           text: $username
         )
         .textFieldStyle(.roundedBorder)
+        .onSubmit {
+          connect()
+        }
+
         Button {
           username = RandomTextSelector(fileName: "usernames")
             .getRandomListElement()
@@ -28,22 +43,22 @@ struct LobbyView: View {
         }
         .help("Select a random usernaem")
       }
-      
-        
+
       Button {
-        GameSession.join(userName: username)
-        currentView = .loadingScreen
+        connect()
       } label: {
         Text("Join")
           .frame(maxWidth: .infinity)
       }
       .disabled(username.isEmpty)
       .buttonStyle(.borderedProminent)
-      .tint(username.isEmpty ? .white : .blue)
+      .tint(username.isEmpty ? .white : .accentColor)
       .help(username.isEmpty ? "You must enter a username" : "")
+
     }
-    
     .controlSize(.large)
-    .frame(maxWidth: 180)
+    .frame(maxWidth: 220)
+    .navigationTitle("Join")
+
   }
 }
