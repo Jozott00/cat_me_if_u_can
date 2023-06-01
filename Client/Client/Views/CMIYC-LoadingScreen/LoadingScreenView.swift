283915c6c5a92b67b9fa974bc9b0c6eb0097e32c
flofriday <flohacksfriday@gmail.com>
2023-06-01 23:35:19 +0200

    Improve the Lobby Screen
    This change has less UX improvement and is mostly a visual polish,
    however the column size is the same as before and should aid the user
    when the layout changes.

--- Client/Client/Views/CMIYC-LoadingScreen/LoadingScreenView.swift
+++ Client/Client/Views/CMIYC-LoadingScreen/LoadingScreenView.swift
@@ -12,24 +12,45 @@ struct LoadingScreenView: View {
   @EnvironmentObject var data: GameData
   var body: some View {
     VStack {
-      Text("Users in Lobby:")
-      if let users = data.activeUsers {
-        ForEach(users, id: \.id) { user in
-          Text(user.name)
+      Text("Users in Lobby")
+        .font(.largeTitle)
+        .bold()
+
+      List {
+        if let users = data.activeUsers {
+          ForEach(Array(users.enumerated()), id: \.offset) { index, user in
+            Text("\(index + 1). \(user.name)")
+              .listRowSeparator(.visible)
+          }
         }
       }
+      .cornerRadius(10.0)
+      .frame(maxHeight: 140)
+      .overlay(
+        RoundedRectangle(cornerRadius: 10)
+          .inset(by: 1)
+          .stroke(.separator, lineWidth: 1)
+      )
 
       Button(
-        "Start the game",
         action: {
           GameSession.gameStart()
+        },
+        label: {
+          Text("Start the game")
+            .frame(maxWidth: .infinity)
         }
       )
+      .buttonStyle(.borderedProminent)
+      .tint(.accentColor)
     }
     .onChange(of: data.gameState) { gameState in
       if gameState != nil {
         currentView = .board
       }
     }
+    .controlSize(.large)
+    .navigationTitle("Lobby")
+    .frame(maxWidth: 220)
   }
 }
